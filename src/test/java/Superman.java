3396e0775444c4211f21b9a2e4e04b356b9e87e9
kevser87 <kevsireadlig87@outlook.com>
2020-11-01 10:37:25 -0500

    initinalize
    (cherry picked from commit bbc43cc609346984c0cf2a0a1f1856bd399ac291)

--- src/test/java/Superman.java
+++ src/test/java/Superman.java
@@ -1,10 +1,7 @@
 public class Superman {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Superman");
-=======
 
->>>>>>> bbc43cc... initinalize
+        System.out.println("Superman");
     }
 
 }
