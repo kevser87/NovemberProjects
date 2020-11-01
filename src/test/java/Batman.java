6a9eaea69b468ee9b32554eb6c6a2d6faea676db
kevser87 <kevsireadlig87@outlook.com>
2020-11-01 10:37:25 -0500

    initinalize
    (cherry picked from commit bbc43cc609346984c0cf2a0a1f1856bd399ac291)

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -5,7 +5,6 @@ public class Batman {
         System.out.println("I will catch Joker");
 
         //Yes he will catch him
-    }
+    }}
 
 
-}
--- src/test/java/Superman.java
+++ src/test/java/Superman.java
@@ -1,6 +1,10 @@
 public class Superman {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Superman");
+=======
+
+>>>>>>> bbc43cc... initinalize
     }
 
 }
